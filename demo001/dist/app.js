4099c9e06973a3a78f3d386064c26a5f4ae3c81c
Vegar Ringdal <vegar.ringdal@gmail.com>
2016-03-27 21:24:35 +0200

    added goto github

--- demo001/dist/app.js
+++ demo001/dist/app.js
@@ -18,7 +18,7 @@ System.register([], function (_export, _context) {
         }
 
         App.prototype.configureRouter = function configureRouter(config, router) {
-          config.title = 'v-grid aurelia element';
+          config.title = '<-goto github';
           config.map([{ route: ['', 'sample01'], name: 'sample01', moduleId: './samples/sample01', nav: true, title: 'col&internal functions' }]);
 
           this.router = router;
@@ -31,4 +31,4 @@ System.register([], function (_export, _context) {
     }
   };
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztxQkFBYTs7Ozs7c0JBQ1gsMkNBQWdCLFFBQVEsUUFBTztBQUM3QixpQkFBTyxLQUFQLEdBQWUsd0JBQWYsQ0FENkI7QUFFN0IsaUJBQU8sR0FBUCxDQUFXLENBQ1QsRUFBRSxPQUFPLENBQUMsRUFBRCxFQUFJLFVBQUosQ0FBUCxFQUF3QixNQUFNLFVBQU4sRUFBa0IsVUFBVSxvQkFBVixFQUFpQyxLQUFLLElBQUwsRUFBVyxPQUFNLHdCQUFOLEVBRC9FLENBQVgsRUFGNkI7O0FBUTdCLGVBQUssTUFBTCxHQUFjLE1BQWQsQ0FSNkI7OztlQURwQiIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VSb290IjoiL3NyYyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztxQkFBYTs7Ozs7c0JBQ1gsMkNBQWdCLFFBQVEsUUFBTztBQUM3QixpQkFBTyxLQUFQLEdBQWUsZUFBZixDQUQ2QjtBQUU3QixpQkFBTyxHQUFQLENBQVcsQ0FDVCxFQUFFLE9BQU8sQ0FBQyxFQUFELEVBQUksVUFBSixDQUFQLEVBQXdCLE1BQU0sVUFBTixFQUFrQixVQUFVLG9CQUFWLEVBQWlDLEtBQUssSUFBTCxFQUFXLE9BQU0sd0JBQU4sRUFEL0UsQ0FBWCxFQUY2Qjs7QUFTN0IsZUFBSyxNQUFMLEdBQWMsTUFBZCxDQVQ2Qjs7O2VBRHBCIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZVJvb3QiOiIvc3JjIn0=
--- demo001/src/app.js
+++ demo001/src/app.js
@@ -1,8 +1,9 @@
 export class App {
   configureRouter(config, router){
-    config.title = 'v-grid aurelia element';
+    config.title = '<-goto github';
     config.map([
       { route: ['','sample01'], name: 'sample01', moduleId: './samples/sample01',  nav: true, title:'col&internal functions' }
+
 
 
     ]);
