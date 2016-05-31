7b5d860978a1958d173c5643cf71d19495755850
Vegar Ringdal <vegar.ringdal@gmail.com>
2016-05-31 23:00:57 +0200

    remove old demo

--- viewSlotsDemo001/jspm_packages/npm/aurelia-framework@1.0.0-beta.1.2.1.js
+++
@@ -1,3 +0,0 @@
-define(["npm:aurelia-framework@1.0.0-beta.1.2.1/aurelia-framework"], function(main) {
-  return main;
-});
--- viewSlotsDemo001/jspm_packages/npm/aurelia-loader-default@1.0.0-beta.1.2.0.js
+++
@@ -1,3 +0,0 @@
-define(["npm:aurelia-loader-default@1.0.0-beta.1.2.0/aurelia-loader-default"], function(main) {
-  return main;
-});
