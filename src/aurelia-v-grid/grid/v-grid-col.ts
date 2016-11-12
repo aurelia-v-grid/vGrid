82c4333934f6795c5a1a8d05b8cfb7d2b9fff9ed
Vegar Ringdal <vegar.ringdal@gmail.com>
2016-11-12 19:06:59 +0100

    chore(all) improve types

--- src/aurelia-v-grid/grid/v-grid-col.ts
+++ src/aurelia-v-grid/grid/v-grid-col.ts
@@ -1,20 +1,27 @@
-import {inject, noView, customElement, bindable, processContent, TargetInstruction} from 'aurelia-framework';
-import {VGrid} from './v-grid';
+import { inject, noView, customElement, bindable, processContent, TargetInstruction } from 'aurelia-framework';
+import { VGrid } from './v-grid';
+// for typings
+import { ViewCompiler, ViewResources} from 'aurelia-framework';
+
 
 @noView()
-@processContent((compiler, resources, element, instruction) => {
+@processContent((
+  compiler: ViewCompiler,
+  resources: ViewResources,
+  element: HTMLElement,
+  instruction: TargetInstruction) => {
 
 
   let headerTemplateElement = element.getElementsByTagName('V-HEADER-TEMPLATE')[0];
   let headerTemplateHtml = headerTemplateElement ? headerTemplateElement.innerHTML : null;
   if (headerTemplateHtml !== '') {
-    instruction.colHeaderTemplate = headerTemplateHtml;
+    (instruction as any).colHeaderTemplate = headerTemplateHtml;
   }
 
   let rowTemplateElement = element.getElementsByTagName('V-ROW-TEMPLATE')[0];
   let rowTemplateHtml = rowTemplateElement ? rowTemplateElement.innerHTML : null;
   if (rowTemplateHtml !== '') {
-    instruction.colRowTemplate = rowTemplateHtml;
+    (instruction as any).colRowTemplate = rowTemplateHtml;
   }
 
   element.innerHTML = '';
@@ -22,7 +29,7 @@ import {VGrid} from './v-grid';
   // we want to get this css attribute and use if later
   let css = element.getAttribute('col-css');
   if (css) {
-    instruction.colCss = css;
+    (instruction as any).colCss = css;
   }
 
 
@@ -39,26 +46,26 @@ export class VGridElementColConfig {
 
 
 
-  @bindable({attribute: 'col-width'}) private colWidth: number;
-  @bindable({attribute: 'col-field'}) private colField: string;
-  @bindable({attribute: 'col-header-name'}) private colHeaderName: string;
-  @bindable({attribute: 'col-sort'}) private colSort: string;
-  @bindable({attribute: 'col-pin-left'}) private colPinLeft: boolean;
-  @bindable({attribute: 'col-pin-right'}) private colPinRight: boolean;
-  @bindable({attribute: 'col-filter'}) private colFilter: string;
-  @bindable({attribute: 'col-filter-top'}) private colFilterTop: boolean;
-  @bindable({attribute: 'col-add-label-attributes'}) private colAddLabelAttributes: string;
-  @bindable({attribute: 'col-add-filter-attributes'}) private colAddFilterAttributes: string;
-  @bindable({attribute: 'col-add-row-attributes'}) private colAddRowAttributes: string;
-  @bindable({attribute: 'col-type'}) private colType: string;
+  @bindable({ attribute: 'col-width' }) private colWidth: number;
+  @bindable({ attribute: 'col-field' }) private colField: string;
+  @bindable({ attribute: 'col-header-name' }) private colHeaderName: string;
+  @bindable({ attribute: 'col-sort' }) private colSort: string;
+  @bindable({ attribute: 'col-pin-left' }) private colPinLeft: boolean;
+  @bindable({ attribute: 'col-pin-right' }) private colPinRight: boolean;
+  @bindable({ attribute: 'col-filter' }) private colFilter: string;
+  @bindable({ attribute: 'col-filter-top' }) private colFilterTop: boolean;
+  @bindable({ attribute: 'col-add-label-attributes' }) private colAddLabelAttributes: string;
+  @bindable({ attribute: 'col-add-filter-attributes' }) private colAddFilterAttributes: string;
+  @bindable({ attribute: 'col-add-row-attributes' }) private colAddRowAttributes: string;
+  @bindable({ attribute: 'col-type' }) private colType: string;
 
 
-  constructor(element: Element, vGrid: VGrid, targetInstruction: any) {
+  constructor(element: Element, vGrid: VGrid, targetInstruction: TargetInstruction) {
     this.vGrid = vGrid;
     this.element = element;
-    this.colRowTemplate = targetInstruction.elementInstruction.colRowTemplate;
-    this.colHeaderTemplate = targetInstruction.elementInstruction.colHeaderTemplate;
-    this.colCss = targetInstruction.elementInstruction.colCss;
+    this.colRowTemplate = (targetInstruction.elementInstruction as any).colRowTemplate;
+    this.colHeaderTemplate = (targetInstruction.elementInstruction as any).colHeaderTemplate;
+    this.colCss = (targetInstruction.elementInstruction as any).colCss;
   }
 
 
