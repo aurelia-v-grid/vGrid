304ccdb155d999adf1e6b1a338228d690cb579ec
Vegar Ringdal <vegar.ringdal@gmail.com>
2016-11-16 19:31:42 +0100

    chore(sample) simple column bind sample

---
+++ sample/src/5k-rows-columnbind.ts
@@ -0,0 +1,41 @@
+import { autoinject } from 'aurelia-framework';
+import { GridConnector } from 'aurelia-v-grid';
+import { DataSource } from 'aurelia-v-grid';
+import { Selection, ColConfig } from 'aurelia-v-grid';
+import { DummyDataGenerator } from './utils/dummyDataGenerator';
+
+@autoinject()
+export class Welcome {
+
+  public ds: DataSource;
+  public gridConnector: GridConnector;
+  private myCollection: any;
+  private myColumns: Array<ColConfig>;
+  constructor(public dummyDataGenerator: DummyDataGenerator) {
+    this.dummyDataGenerator.generateData(5000, (data) => {
+      this.myCollection = data;
+    });
+    this.ds = new DataSource(new Selection('multiple'));
+    this.gridConnector = new GridConnector(this.ds);
+
+    this.ds.setArray(this.myCollection);
+
+    this.myColumns = ([
+      {
+        colWidth: 100,
+        colField: 'name'
+        colPinLeft: true
+      },
+      {
+        colWidth: 100,
+        colField: 'index'
+      }
+
+    ] as Array<ColConfig>);
+
+  }
+
+
+
+
+}
