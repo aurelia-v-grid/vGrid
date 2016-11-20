780150f9c8ca5612e2a4473f6505aa3635cad022
Vegar Ringdal <vegar.ringdal@gmail.com>
2016-11-20 18:07:27 +0100

    chore(all) new build

--- dist/es2015/grid/groupingElements.js
+++ dist/es2015/grid/groupingElements.js
@@ -33,7 +33,7 @@ define(["require", "exports", "aurelia-framework"], function (require, exports, 
                 this.lastAdded = name;
                 this.groupContext[name] = new GroupContext(name, field, this);
                 var viewMarkup = this.colGroupElement ||
-                    "<div class=\"avg-grouping\">\n                    <p v-sort=\"field:" + field + "\">" + name + "</p>\n                    <p>&nbsp;&nbsp;\n                        <i click.delegate=\"remove()\" class=\"avg-fa avg-fa-times-circle-o\" aria-hidden=\"true\"></i>\n                    </p>\n                </div>";
+                    "<div class=\"avg-grouping\">\n          <p v-sort=\"field:" + field + "\">" + name + "</p>\n          <p>\n            <svg click.delegate=\"remove()\" class=\"icon\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\">\n              <path d=\"M1.00047418 13.4679717l12.8276448-12.45678394 1.23390682 1.55209136L2.23438102 15.02006306z\"/>\n              <path d=\"M15.06202587 13.49922255L2.23438107 1.0424386 1.00047426 2.59452998 13.82811903 15.0513139z\"/>\n            </svg>\n          </p>\n         </div>";
                 var viewFactory = this.viewCompiler.compile("<template>" + viewMarkup + "</template>", this.viewResources);
                 var view = viewFactory.create(this.container);
                 var viewSlot = new aurelia_framework_1.ViewSlot(this.avgTopPanel, true);
