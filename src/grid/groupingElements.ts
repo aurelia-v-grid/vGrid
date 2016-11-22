5e718e5a9b78f05fbf30002720eb9620401eaddb
Vegar Ringdal <vegar.ringdal@gmail.com>
2016-11-22 19:11:18 +0100

    chore(grouping) only display remove when hovering

--- src/grid/groupingElements.ts
+++ src/grid/groupingElements.ts
@@ -89,13 +89,12 @@ export class GroupingElements {
 
       // view-viewslot
       let viewMarkup = this.colGroupElement ||
-        `<div class="avg-grouping">
-          <p v-sort="field:${field}">${name}<svg click.delegate="remove()" class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+        `<div class="avg-grouping">  
+          <p class="avg-grouping-element" v-sort="field:${field}">${name} 
+            <i><svg click.delegate="remove()" class="icon iconhidden" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
               <path d="M3 4l4.3 4L3 12h1.4L8 8.7l3.5 3.3H13L8.6 8 13 4h-1.5L8 7.3 4.4 4H3z"/>
-            </svg></p>
-
-            
-
+            </svg></i>
+          </p>
          </div>`;
 
 
