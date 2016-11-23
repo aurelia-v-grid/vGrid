6e8383795ebbb387143b7d5b1445d1e6fada80f5
Vegar Ringdal <vegar.ringdal@gmail.com>
2016-11-23 20:28:47 +0100

    fix(custom grouping element)

--- src/grid/groupingElements.ts
+++ src/grid/groupingElements.ts
@@ -88,21 +88,14 @@ export class GroupingElements {
       this.groupContext[name] = new GroupContext(name, field, this);
 
       // view-viewslot
-      let viewMarkup =
+      let viewMarkup = this.colGroupElement ||
         `<div class="avg-grouping">  
-          <p class="avg-grouping-element" v-sort="field:${field}">${name} 
+          <p class="avg-grouping-element" v-sort="field.bind:field">${name} 
             <i><svg click.delegate="remove()" class="icon iconhidden" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
               <path d="M3 4l4.3 4L3 12h1.4L8 8.7l3.5 3.3H13L8.6 8 13 4h-1.5L8 7.3 4.4 4H3z"/>
             </svg></i>
           </p>
          </div>`;
-
-
-      if (this.colGroupElement) {
-        // if custom we need to string interpolate it   
-        const tpl = new Function('field', 'name', 'return `' + this.colGroupElement + '`;');
-        viewMarkup = tpl.call(this, field, name);
-      }
 
 
       let viewFactory = this.viewCompiler.compile(`<template>${viewMarkup}</template>`, this.viewResources);
