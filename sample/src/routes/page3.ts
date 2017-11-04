0a78fe5e883837adecb718f9a02bc95c5f85876d
Vegar ringdal <vegar.ringdal@gmail.com>
2017-11-04 15:32:10 +0100

    chore sample

--- sample/src/routes/page3.ts
+++ sample/src/routes/page3.ts
@@ -1,12 +1,36 @@
 import { Router } from 'aurelia-router';
 import { autoinject } from 'aurelia-framework';
+import { GridConnector } from 'aurelia-v-grid';
+import { DataSource } from 'aurelia-v-grid';
+import { Selection } from 'aurelia-v-grid';
+import { DummyDataGenerator } from '../utils/dummyDataGenerator';
 
 @autoinject(Router)
-export class Page3 {
+export class Page2 {
     public title: string;
+    public ds: DataSource;
+    public gridConnector: GridConnector;
+    private dsEventID: number;
+    private testString: string;
+    private myCollection: any;
+    private columns: any;
+    private showgrid: any;
 
-    constructor(public router: Router) {
+    constructor(public router: Router, public dummyDataGenerator: DummyDataGenerator) {
+        this.dummyDataGenerator.generateData(300, (data: any) => {
+            this.myCollection = data;
+        });
 
+        this.showgrid = true;
+
+        this.ds = new DataSource(new Selection('multiple'), {
+
+        });
+
+
+        this.gridConnector = new GridConnector(this.ds);
+        this.gridConnector.setInitTop(this.dummyDataGenerator.rowTop);
+        this.ds.setArray(this.myCollection);
     }
 
     public canActivate(a, b, c) {
