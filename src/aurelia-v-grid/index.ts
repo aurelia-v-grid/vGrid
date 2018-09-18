d88e5202bd4d5e76928859c19f5e6ad7f9913659
Vegar Ringdal <vegar.ringdal@gmail.com>
2018-09-18 20:38:49 +0200

    refactor main export from vgrid to globalResources

--- src/aurelia-v-grid/index.ts
+++ src/aurelia-v-grid/index.ts
@@ -19,7 +19,7 @@ import { VGridContextmenu } from './grid/v-grid-contextmenu';
 import { VGridFooter } from './grid/v-grid-footer';
 import { VGridElementColConfig } from './grid/v-grid-col';
 
-export const vgridClasses: any = [
+export const globalResources: any = [
   VGrid,
   VGridAttributesFilter,
   VGridAttributesFilterObserver,
@@ -40,28 +40,3 @@ export const vgridClasses: any = [
   VGridElementColConfig
 ];
 
-
-/*
-const prefix = './grid';
-export function configure(config: any) {
-  config.globalResources(
-    prefix + '/attributes/v-filter',
-    prefix + '/attributes/v-filter-observer',
-    prefix + '/attributes/v-sort',
-    prefix + '/attributes/v-image',
-    prefix + '/attributes/v-drag-drop-col',
-    prefix + '/attributes/v-changed',
-    prefix + '/attributes/v-data-handler',
-    prefix + '/attributes/v-resize-col',
-    prefix + '/attributes/v-menu',
-    prefix + '/attributes/v-selection',
-    prefix + '/v-grid-row-repeat',
-    prefix + '/v-grid-group-row',
-    prefix + '/v-grid-group-element',
-    prefix + '/v-grid-loadingscreen',
-    prefix + '/v-grid-contextmenu',
-    prefix + '/v-grid-footer',
-    prefix + '/v-grid-col',
-    prefix + '/v-grid'
-  );
-} */
--- src/sample/main.ts
+++ src/sample/main.ts
@@ -1,11 +1,11 @@
 import { BasicConfiguration } from '@aurelia/jit';
-import { vgridClasses } from 'aurelia-v-grid';
+import { globalResources } from 'aurelia-v-grid';
 import { Aurelia } from '@aurelia/runtime';
 import { App } from './app';
 
 // how does vNext register a plugin ?
 
 window['au'] = new Aurelia()
-  .register(BasicConfiguration, ...vgridClasses)
+  .register(BasicConfiguration, ...globalResources)
   .app({ host: document.querySelector('app'), component: new App() })
   .start();
